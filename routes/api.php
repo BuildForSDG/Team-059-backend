ae3e262084c5e0725dd6a82a351be4a959f01280
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-16 02:19:28 +0100

    added route structure

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,91 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| api Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register api routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "api" middleware group. Enjoy building your api!
 |
 */
+######################## VERSION 1 ##############################
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+ 
+//User/Teacher routing group
+Route::prefix('v1/user')->middleware([])->group(function(){
+
+    Route::get('/',function(){
+        return response()->json(['details'=>'User endpoint']);
+    });
+
+    Route::post('/create','api\v1\Users\UserController@create');
+    Route::post('/login','api\v1\Users\UserController@login');
+
+
+    //protect user endpoints
+     Route::middleware([/*'auth:api'*/])->group(
+function(){
+   
+
+
+}
+
+     );
+    
+    });
+
+
+ 
+//Company/Schools routing group
+Route::prefix('v1/company')->middleware([/*'auth:api'*/])->group(function(){
+
+    Route::get('/',function(){
+    
+        return response()->json(['details'=>'Company endpoint']);
+    
+    });
+
+
+    //protect company endpoints
+     Route::middleware([])->group(
+function(){
+   
+
+
+}
+
+     );
+    
+    });
+    
+ 
+
+    
+//Admin routing group
+Route::prefix('v1/admin')->middleware([/*'auth:api'*/])->group(function(){
+
+    Route::get('/',function(){
+    
+        return response()->json(['details'=>'Admin endpoint']);
+    
+    });
+
+
+    //protect admin endpoints
+     Route::middleware([])->group(
+function(){
+   
+
+
+}
+
+     );
+    
+    });
+
+
+######################## VERSION 1 ROUTING ENDS ##############################
